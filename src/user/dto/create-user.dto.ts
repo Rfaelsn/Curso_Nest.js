29c291dc76392ff487bfaa69151910d3874f7cc6
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-05-14 15:30:50 -0300

    criação do crud com prisma

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsEmail,
+  IsStrongPassword,
+  IsString,
+  IsOptional,
+  IsDateString,
+} from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsDateString()
+  birthAt: string;
+
+  @IsStrongPassword({
+    minLength: 6,
+    minUppercase: 1,
+    minSymbols: 1,
+  })
+  password: string;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,93 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateUserDTO) {
+    return this.prisma.user.create({
+      data,
+      //   select: { id: true }
+    });
+  }
+
+  async list() {
+    return this.prisma.user.findMany();
+  }
+
+  async show(id: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(
+    id: number,
+    { name, email, password, birthAt }: UpdatePutUserDTO,
+  ) {
+    await this.exists(id);
+    return this.prisma.user.update({
+      data: {
+        name,
+        email,
+        password,
+        birthAt: birthAt ? new Date(birthAt) : null,
+      },
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updatePartial(
+    id: number,
+    { name, email, password, birthAt }: UpdatePatchUserDTO,
+  ) {
+    await this.exists(id);
+    const data: any = {};
+
+    if (birthAt) {
+      data.birthAt = new Date(birthAt);
+    }
+
+    if (name) {
+      data.name = name;
+    }
+
+    if (email) {
+      data.email = email;
+    }
+
+    if (password) {
+      data.password = password;
+    }
+
+    return this.prisma.user.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  async delete(id: number) {
+    await this.exists(id);
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async exists(id: number) {
+    if (!(await this.show(id))) {
+      throw new NotFoundException(`O usuário de id ${id} nao existe`);
+    }
+  }
+}
